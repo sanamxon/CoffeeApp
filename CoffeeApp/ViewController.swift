73a7bf51529b2e9acd9a013857b4b5374fd5a386
shokhsanam <sanamxon1994@gmail.com>
2017-11-02 13:20:56 -0700

    commits

--- CoffeeApp/ViewController.swift
+++ CoffeeApp/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var INPUT: UITextField!
+    @IBOutlet weak var PAYPRICE: UILabel!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        INPUT.layer.cornerRadius = 30.0
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var CALCULATE: UIButton!
+    
+    @IBAction func calculateTapped(_ sender: Any) {
+        
+        if INPUT.text == ""
+        {
+            errorLabel.text = "Enter number to text field"
+        }
+            
+        else
+    {
+        let input1 = Double(INPUT.text!)
+        PAYPRICE.text = "$ \(input1! * 5)"
+    }
+    
+    
 }
 
+}
